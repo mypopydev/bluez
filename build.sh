050f881201e4f11a16664af992f74aa70aa8a576
Jun Zhao <mypopydev@gmail.com>
2017-03-12 14:51:14 +0800

    Add apt-get install.

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
+#
+# apt-get install libusb-dev libdbus-1-dev libglib2.0-dev automake libudev-dev libical-dev libreadline-dev libbluetooth-dev
+# apt-get install emacs emacs24-el
 ./bootstrap && ./configure --enable-debug --enable-static --enable-pie --enable-test && make
