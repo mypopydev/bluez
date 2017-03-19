6aba7f126e5b730b50b68ae5b2b8956a7e959e3c
Jun Zhao <mypopydev@gmail.com>
2017-03-19 18:57:44 +0800

    Add raspberry pi3 support

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #
-# apt-get install libusb-dev libdbus-1-dev libglib2.0-dev automake libudev-dev libical-dev libreadline-dev libbluetooth-dev
+# apt-get install libusb-dev libdbus-1-dev libglib2.0-dev automake libudev-dev libical-dev libreadline-dev libbluetooth-dev libpcap-dev
 # apt-get install emacs emacs24-el
+# systemctl status gattclient.service
+# systemctl enable gattclient.service
+# systemctl start gattclient.service
+# systemctl status bluetoothctl.service
+# systemctl enable bluetoothctl.service
+# systemctl start bluetoothctl.service
 # gatttool -b 88:C2:55:BC:73:AF --char-write-req --handle=0x0013 --value=0200 --listen
 ./bootstrap && ./configure --enable-debug --enable-static --enable-pie --enable-test && make
